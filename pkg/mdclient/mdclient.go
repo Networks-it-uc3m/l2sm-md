469b251e1cdf35c7af6a0043038e8eaca34f99dd
Alex de Cock Buning <100383348@alumnos.uc3m.es>
2024-10-08 01:51:29 +0200

    implemented create and delete network
    Using k8s client, when server receives a grpc request, a network can be manipulated in multiple clusters

--- pkg/mdclient/mdclient.go
+++ pkg/mdclient/mdclient.go
@@ -3,19 +3,61 @@ package mdclient
 import (
 	"fmt"
 
+	"context"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
 	"l2sm.local/l2sm-md/pkg/pb"
 )
 
-func CreateNetwork(network *pb.L2Network) error {
+type MDClient struct {
+	ClusterConfigs []rest.Config
+}
 
-	fmt.Printf("Received network %s", network.GetName())
+func (mdcli *MDClient) CreateNetwork(network *pb.L2Network) error {
+
+	fmt.Printf("Creating network %s", network.GetName())
+
+	for _, clusterConfig := range mdcli.ClusterConfigs {
+
+		dynClient, err := dynamic.NewForConfig(&clusterConfig)
+		if err != nil {
+			return fmt.Errorf("Error contacting cluster %s: %v\n", clusterConfig.String(), err)
+		}
+
+		_, err = dynClient.Resource(schema.GroupVersionResource{Group: "l2sm.l2sm.k8s.local", Version: "v1", Resource: "l2smnetworks"}).Namespace("default").Create(context.Background(), obj, metav1.CreateOptions{})
+		if err != nil {
+			return fmt.Errorf("Error creating resource: %v\n", err)
+		}
+
+		return nil
+	}
 
 	return nil
 }
 
-func DeleteNetwork(network string) error {
+func (mdcli *MDClient) DeleteNetwork(network string) error {
 
 	fmt.Printf("Deleting network %s", network)
 
+	for _, clusterConfig := range mdcli.ClusterConfigs {
+
+		dynClient, err := dynamic.NewForConfig(&clusterConfig)
+		if err != nil {
+			return fmt.Errorf("Error contacting cluster %s: %v\n", clusterConfig.String(), err)
+		}
+
+		_, err = dynClient.Resource(schema.GroupVersionResource{Group: "l2sm.l2sm.k8s.local", Version: "v1", Resource: "l2smnetworks"}).Namespace("default").Create(context.Background(), obj, metav1.CreateOptions{})
+		if err != nil {
+			return fmt.Errorf("Error creating resource: %v\n", err)
+		}
+
+		return nil
+	}
+
 	return nil
+
 }

2dd1a6f89d67673acf0af2edd54c036713d3fc19
Alex de Cock Buning <100383348@alumnos.uc3m.es>
2024-10-09 06:51:02 +0200

    added l2smv1 api to the dynamic client request

--- pkg/mdclient/mdclient.go
+++ pkg/mdclient/mdclient.go
@@ -6,11 +6,14 @@ import (
 	"context"
 	"fmt"
 
+	l2smv1 "github.com/Networks-it-uc3m/L2S-M/api/v1"
+	"github.com/Networks-it-uc3m/l2sm-md/pkg/pb"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
-	"l2sm.local/l2sm-md/pkg/pb"
 )
 
 type MDClient struct {
@@ -21,14 +24,19 @@ func (mdcli *MDClient) CreateNetwork(network *pb.L2Network) error {
 
 	fmt.Printf("Creating network %s", network.GetName())
 
+	decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+	obj := &unstructured.Unstructured{}
+	decUnstructured.Decode(yamlContent, nil, obj)
+
 	for _, clusterConfig := range mdcli.ClusterConfigs {
 
 		dynClient, err := dynamic.NewForConfig(&clusterConfig)
 		if err != nil {
 			return fmt.Errorf("Error contacting cluster %s: %v\n", clusterConfig.String(), err)
 		}
+		resource := l2smv1.GroupVersion.WithResource("l2networks")
 
-		_, err = dynClient.Resource(schema.GroupVersionResource{Group: "l2sm.l2sm.k8s.local", Version: "v1", Resource: "l2smnetworks"}).Namespace("default").Create(context.Background(), obj, metav1.CreateOptions{})
+		_, err = dynClient.Resource(resource).Namespace("default").Create(context.Background(), obj, metav1.CreateOptions{})
 		if err != nil {
 			return fmt.Errorf("Error creating resource: %v\n", err)
 		}

d4ea0ab635b4b04a9a463e2036d426034d486e67
Alex de Cock Buning <100383348@alumnos.uc3m.es>
2025-03-04 17:05:17 +0100

    added env variable field

--- pkg/l2sminterface/l2sminterface.go
+++ pkg/l2sminterface/l2sminterface.go
@@ -1,6 +1,10 @@
 package l2sminterface
 
-import "github.com/Networks-it-uc3m/l2sm-md/api/v1/l2smmd"
+import (
+	"fmt"
+
+	"github.com/Networks-it-uc3m/l2sm-md/api/v1/l2smmd"
+)
 
 func GetWorkloadPatchInstructions(networkName string) []*l2smmd.FieldPatch {
 
@@ -8,8 +12,8 @@ func GetWorkloadPatchInstructions(networkName string) []*l2smmd.FieldPatch {
 		{Path: "spec.template.metadata.labels.l2sm", Value: "true"},
 		{Path: "spec.template.metadata.labels.l2sm/app", Value: "<workload-name>"},
 		{Path: "spec.template.metadata.annotations.l2sm/networks", Value: networkName},
-		// l2smmd.FieldPatch{Path: "a",Value: "b" },
-
+		{Path: "spec.template.spec.containers[0].env[0].name", Value: "DNS_NAME"},
+		{Path: "spec.template.spec.containers[0].env[0].value", Value: fmt.Sprintf("<workload-name>.%s.%s.l2sm", networkName, "inter")},
 	}
 	return patches
 }
